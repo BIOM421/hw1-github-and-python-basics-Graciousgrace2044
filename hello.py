87815a7c8107b4bba309b25441117801763c785e
Graciousgrace2044 <graceeh123@gmail.com>
2024-05-27 09:08:35 -0600

    hw 1
    hw 1

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
 def hello_world():
-	return "Hello"
-"hello_world_n(N)" 
+    return "Hello World!"
+
+def hello_world_n(N):
+    return " ".join(["Hello World!"] * N)
+print(hello_world())
+print(hello_world_n(3))
 
